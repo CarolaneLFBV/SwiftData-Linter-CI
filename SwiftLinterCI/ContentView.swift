756abab8b02735e6e6fbf19a1ee7a2cd1985a8ba
CarolaneLFBV <carolane_lefebvre@icloud.com>
2024-10-16 19:47:04 +0200

    initial commit

---
+++ SwiftLinterCI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftLinterCI
+//
+//  Created by Carolane Lefebvre on 16/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            
+            Text("Yes")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftLinterCI/SwiftLinterCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLinterCIApp.swift
+//  SwiftLinterCI
+//
+//  Created by Carolane Lefebvre on 16/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLinterCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
